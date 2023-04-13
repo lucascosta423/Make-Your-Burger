d510a6c1c0f59455ff55de2916d80712b582b820
Lukas Stranges <lukascosta.ls17@gmail.com>
2023-04-13 11:55:09 -0300

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Make-Your-Burger/'
-    : '/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
